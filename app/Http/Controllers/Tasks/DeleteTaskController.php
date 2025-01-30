0b8a0f80d7a8c32cbee5ddbb2bd6781a1200ba8f
antonio <antonio.bolano@web.de>
2025-01-30 23:09:16 +0100

    feat(task): implement delete action

--- app/Http/Controllers/Tasks/DeleteTaskController.php
+++ app/Http/Controllers/Tasks/DeleteTaskController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers\Tasks;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
+use function PHPUnit\Framework\isEmpty;
 
 class DeleteTaskController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(int $task)
     {
-        //
+        $task = Task::find($task);
+
+        if (!$task) {
+            return response('', 404);
+        }
+
+        $task->delete();
+        return response('', 204);
     }
 }
